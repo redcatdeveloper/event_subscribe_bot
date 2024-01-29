6e8d306e0fc035c8fa4407064a117d7f195f2b31
Vladimir Ignatenko <redcatdeveloper@gmail.com>
2024-01-29 23:46:39 +0300

    Start

---
+++ src/go.mod
@@ -0,0 +1,7 @@
+module github.com/redcatdeveloper/event_subscribe_bot
+
+go 1.18
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+
+require github.com/redcatdeveloper/goconf v1.0.2
---
+++ src/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "strconv"
+  tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+  goconf "github.com/redcatdeveloper/goconf"
+)
+
+func main() {
+  conf, err := goconf.NewGoConf("config.ini")
+  if err != nil {
+    log.Panic(err)
+  }
+
+  log.Printf("Usage Token: %s \n", conf.Get("Token"))
+  DebugChannelId, _ := strconv.ParseInt(conf.Get("DebugChannelId"), 10, 64)
+
+	bot, err := tgbotapi.NewBotAPI(conf.Get("Token"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			debugLog(bot, DebugChannelId, fmt.Sprintf("[%s] on %d: %s", update.Message.From.UserName, update.Message.Chat.ID, update.Message.Text))
+
+      if update.Message.IsCommand() {
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+        switch update.Message.Command() {
+          case "start":
+            msg.Text = "Привет"
+          default:
+            msg.Text = "ой"
+        }
+			  bot.Send(msg)
+      }
+		}
+	}
+}
+
+func debugLog(bot *tgbotapi.BotAPI, channelId int64, str string) {
+  log.Print(str)
+  msg := tgbotapi.NewMessage(channelId, str)
+  bot.Send(msg)
+}
